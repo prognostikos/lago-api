cfd54785088d391b0a04928305d8dee0cb7b963f
Vincent Pochet <vincent.pochet@gmail.com>
2022-06-10 16:48:52 +0200

    Add payment provider to customers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_10_134535) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_10_143942) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -126,6 +126,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_10_134535) do
     t.string "legal_name"
     t.string "legal_number"
     t.float "vat_rate"
+    t.string "payment_provider", default: "lago"
     t.index ["customer_id"], name: "index_customers_on_customer_id"
     t.index ["organization_id"], name: "index_customers_on_organization_id"
   end
